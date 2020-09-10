c8061996a4e54f4a8c5dc3181ec31b57bc55316b
Thomas <thomas.fritschy@gmail.com>
2020-09-10 16:30:27 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# bblutin_menuperso
-Personnal menu build for ESX
+# [BETA] bblutin_menuperso
+ > [EN] A personnal menu for ESX build in [NativeUI](https://github.com/FrazzIe/NativeUILua) and including [DPEmotes](https://github.com/andristum/dpemotes)
+
+ > [FR] Un menu personnel pour ESX fait en [NativeUI](https://github.com/FrazzIe/NativeUILua) et incluant [DPEmotes](https://github.com/andristum/dpemotes)
+
+[![TITRE](https://i.imgur.com/kosIXHU.png)]()
+
+### [EN]
+```
+This script is still in BETA. A lot of improvement will be done on the animation and admin part in the future. 
+If you have suggestions or want to collaborate, feel free to contact me ;)
+```
+
+### [FR]
+```
+Ce script est en BETA. Une grande quantité d'améliorations vont être apportées au niveau des animations et de la partie admin. 
+Si vous avez des suggestions ou voulez collaborer avec moi, n'hésitez pas à me contacter ;)
+```
+
+---
+## ⚡ Installation 
+
+How to get started with the script
+
+### Download
+
+- You can clone this repo to your local machine using :
+```
+cd resources 
+git clone https://github.com/BBlutin/bblutin_menuperso.git
+```
+
+- Or you can download it manually :
+
+  - Download https://github.com/BBlutin/bblutin_menuperso/archive/master.zip
+  - Put it in the [esx] directory
+  
+### Import SQL & Start
+
+- Import `keybinds.sql` in your database
+- Add this to your `server.cfg`:
+```
+ensure bblutin_menuperso
+```
+
+---
+
+## 📺 Preview
+
+[![Aperçu](https://i.imgur.com/SghrOL4.png)](https://streamable.com/guk2hz)
+
+---
+
+## 📋 Structure
+
+### 📂 bblutin_menuperso
+
+- **📂 client**
+
+  - 📄 main.lua
+  - 📄 animlist.lua
+  - 📄 emote.lua
+  - 📄 syncing.lua
+
+- **📂 server**
+
+  - 📄 main.lua
+  - 📄 server.lua
+
+- **📂 dependencies**
+
+    - All the NativeUI stuff
+
+- **📂 html**
+
+    - Files to create the Cinematic overlay
+
+- **📂 locales**
+
+    - 📄 fr.lua
+
+- **📂 stream**
+
+    - Custom Anims
+
+- 📄 __resource.lua
+
+- 📄 config.lua
+
+- 📄 keybinds.sql
+
+---
+## 💜 Special thanks
+
+A big thanks to [Dullpear](https://github.com/andristum) for his great emote script and to [FrazzIe](https://github.com/FrazzIe) for the NativeUI
+
+---
+
+## 🔰 License
+
+Copyright (C) 2020 BBlutin
+
+This program Is free software: you can redistribute it And/Or modify it under the terms Of the GNU General Public License As published by the Free Software Foundation, either version 3 Of the License, Or (at your option) any later version.
+
+This program Is distributed In the hope that it will be useful, but WITHOUT ANY WARRANTY without even the implied warranty Of MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License For more details.
+
+You should have received a copy Of the GNU General Public License along with this program. If Not, see http://www.gnu.org/licenses/.
--- client/Emote.lua
+++ client/Emote.lua
@@ -16,6 +16,68 @@ local PtfxNotif = false
 local PtfxPrompt = false
 local PtfxWait = 500
 local PtfxNoProp = false
+local isInRagdoll = false
+
+
+Citizen.CreateThread(function()
+  while true do
+     Citizen.Wait(10)
+     if isInRagdoll then
+       SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
+     end
+   end
+ end)
+ 
+ Citizen.CreateThread(function()
+     while true do
+     Citizen.Wait(0)
+     if IsControlJustPressed(2, Config.RagdollKeybind) and Config.RagdollEnabled and IsPedOnFoot(PlayerPedId()) then
+         if isInRagdoll then
+             isInRagdoll = false
+         else
+             isInRagdoll = true
+             Wait(500)
+         end
+     end
+   end
+ end)
+
+function WalkMenuStart(name)
+  RequestWalking(name)
+  SetPedMovementClipset(PlayerPedId(), name, 0.2)
+  RemoveAnimSet(name)
+end
+
+function RequestWalking(set)
+  RequestAnimSet(set)
+  while not HasAnimSetLoaded(set) do
+    Citizen.Wait(1)
+  end 
+end
+
+function WalksOnCommand(source, args, raw)
+  local WalksCommand = ""
+  for a in pairsByKeys(DP.Walks) do
+    WalksCommand = WalksCommand .. ""..string.lower(a)..", "
+  end
+  EmoteChatMessage(WalksCommand)
+  EmoteChatMessage("To reset do /walk reset")
+end
+
+function WalkCommandStart(source, args, raw)
+  local name = firstToUpper(args[1])
+
+  if name == "Reset" then
+      ResetPedMovementClipset(PlayerPedId()) return
+  end
+
+  local name2 = table.unpack(DP.Walks[name])
+  if name2 ~= nil then
+    WalkMenuStart(name2)
+  else
+    EmoteChatMessage("'"..name.."' is not a valid walk")
+  end
+end
 
 Citizen.CreateThread(function()
   while true do
@@ -422,3 +484,108 @@ function OnEmotePlay(EmoteName)
   end
   return true
 end
+
+if Config.SqlKeybinding then
+  local emob1 = ""
+  local emob2 = ""
+  local emob3 = ""
+  local emob4 = ""
+  local emob5 = ""
+  local emob6 = ""
+  local keyb1 = ""
+  local keyb2 = ""
+  local keyb3 = ""
+  local keyb4 = ""
+  local keyb5 = ""
+  local keyb6 = "" 
+  local Initialized = false
+  
+  -----------------------------------------------------------------------------------------------------
+  -- Commands / Events --------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------------------------------
+  
+  Citizen.CreateThread(function()
+    while true do
+  
+      if NetworkIsPlayerActive(PlayerId()) and not Initialized then
+          if not Initialized then
+              TriggerServerEvent("dp:ServerKeybindExist")
+              Wait(5000)
+          end
+      end
+  
+      if not IsPedSittingInAnyVehicle(PlayerPedId()) then
+          for k, v in pairs(Config.KeybindKeys) do
+              if IsControlJustReleased(0, v) then
+                  if k == keyb1 then if emob1 ~= "" then EmoteCommandStart(nil,{emob1, 0}) end end
+                  if k == keyb2 then if emob2 ~= "" then EmoteCommandStart(nil,{emob2, 0}) end end
+                  if k == keyb3 then if emob3 ~= "" then EmoteCommandStart(nil,{emob3, 0}) end end
+                  if k == keyb4 then if emob4 ~= "" then EmoteCommandStart(nil,{emob4, 0}) end end
+                  if k == keyb5 then if emob5 ~= "" then EmoteCommandStart(nil,{emob5, 0}) end end
+                  if k == keyb6 then if emob6 ~= "" then EmoteCommandStart(nil,{emob6, 0}) end end
+                  Wait(1000)
+              end
+          end
+      end
+      Citizen.Wait(1)
+    end
+  end)
+  
+  RegisterNetEvent("dp:ClientKeybindExist")
+  AddEventHandler("dp:ClientKeybindExist", function(does)
+      if does then
+        TriggerServerEvent("dp:ServerKeybindGrab")
+      else
+        TriggerServerEvent("dp:ServerKeybindCreate")
+      end
+  end)
+  
+  RegisterNetEvent("dp:ClientKeybindGet")
+  AddEventHandler("dp:ClientKeybindGet", function(k1, e1, k2, e2, k3, e3, k4, e4, k5, e5, k6, e6)
+      keyb1 = k1 emob1 = e1 keyb2 = k2 emob2 = e2 keyb3 = k3 emob3 = e3 keyb4 = k4 emob4 = e4 keyb5 = k5 emob5 = e5 keyb6 = k6 emob6 = e6
+      Initialized = true
+  end)
+  
+  RegisterNetEvent("dp:ClientKeybindGetOne")
+  AddEventHandler("dp:ClientKeybindGetOne", function(key, e)
+      SimpleNotify(Config.Languages[lang]['bound'].."~y~"..e.."~w~ "..Config.Languages[lang]['to'].." ~g~"..firstToUpper(key).."~w~")
+    if key == "num4" then emob1 = e keyb1 = "num4" elseif key == "num5" then emob2 = e keyb2 = "num5" elseif key == "num6" then emob3 = e keyb3 = "num6" elseif key == "num7" then emob4 = e keyb4 = "num7" elseif key == "num8" then emob5 = e keyb5 = "num8" elseif key == "num9" then emob6 = e keyb6 = "num9" end
+  end)
+  
+  -----------------------------------------------------------------------------------------------------
+  ------ Functions and stuff --------------------------------------------------------------------------
+  -----------------------------------------------------------------------------------------------------
+  
+  function EmoteBindsStart()
+      EmoteChatMessage(Config.Languages[lang]['currentlyboundemotes'].."\n"
+          ..firstToUpper(keyb1).." = '^2"..emob1.."^7'\n"
+          ..firstToUpper(keyb2).." = '^2"..emob2.."^7'\n"
+          ..firstToUpper(keyb3).." = '^2"..emob3.."^7'\n"
+          ..firstToUpper(keyb4).." = '^2"..emob4.."^7'\n"
+          ..firstToUpper(keyb5).." = '^2"..emob5.."^7'\n"
+          ..firstToUpper(keyb6).." = '^2"..emob6.."^7'\n")
+  end
+  
+  function EmoteBindStart(source, args, raw)
+      if #args > 0 then
+          local key = string.lower(args[1])
+          local emote = string.lower(args[2])
+          if (Config.KeybindKeys[key]) ~= nil then
+            if DP.Emotes[emote] ~= nil then
+                TriggerServerEvent("dp:ServerKeybindUpdate", key, emote)
+            elseif DP.Dances[emote] ~= nil then
+                TriggerServerEvent("dp:ServerKeybindUpdate", key, emote)
+            elseif DP.PropEmotes[emote] ~= nil then
+                TriggerServerEvent("dp:ServerKeybindUpdate", key, emote)
+            else
+                EmoteChatMessage("'"..emote.."' "..Config.Languages[lang]['notvalidemote'].."")
+            end
+          else
+            EmoteChatMessage("'"..key.."' "..Config.Languages[lang]['notvalidkey'])
+          end
+      else
+          print("invalid")
+      end
+  end
+  
+  end
